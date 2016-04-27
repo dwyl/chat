bf3c1502938eb9f5b9d7d2a8c6aeba32a7de271c
nelsonic <contact.nelsonic+github@gmail.com>
2016-04-27 19:36:48 +0100

    remind me to add some error handling in the next itteration... https://github.com/dwyl/chat/issues/22

--- index.js
+++ index.js
@@ -12,18 +12,15 @@ function handler (event, context) {
       context.succeed(data);
     });
   }
-  else if (event.m) { // SAVE
+  else { // SAVE
     save(event, function(err, data) {
-      if (err) {
-        console.log("Error uploading data: ", err);
-      } else {
+      // if (err) {
+      //   console.log("Error uploading data: ", err);
+      // } else {
         console.log("Successfully uploaded data to ", data.Location);
         return context.succeed(data.Location);
-      }
+      // }
     });
-  }
-  else {
-    context.fail('no message provided')
   }
 
 }
